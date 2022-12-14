79602899d341919a0e7339ba733bfdf0eb980297
KO_Paing_Lay <107101116+Kopaing12@users.noreply.github.com>
2022-12-14 23:30:00 +0630

    Update config.py

--- bot/config/config.py
+++ bot/config/config.py
@@ -30,40 +30,27 @@ def get_config(key: str, default: str = None, is_bool: bool = False) -> Union[st
 
 class Config:
 
-    BOT_TOKEN = get_config("BOT_TOKEN", "5476898127:AAES0EVnVgpxYC8WSc0gXlIaxAHdVvktP38")
+    BOT_TOKEN = get_config("BOT_TOKEN", "5854659368:AAHQmTpR9jEYBhjdBPSVNl0k0WRpk_eJqao")
     API_ID = int(get_config("API_ID", "7880210"))
     API_HASH = get_config("API_HASH", "1bb4b2ff1489cc06af37cba448c8cce9")
 
     DATABASE_URI = get_config("DATABASE_URL", "mongodb+srv://pmbot1:pmbot1@cluster0.esuavhf.mongodb.net/?retryWrites=true&w=majority")
-    SESSION_NAME = get_config("DATABASE_NAME", "FILTER2_BOT")
-    COLLECTION_NAME = get_config("COLLECTION_NAME", "FILTERS")
-    GROUPDB_NAME = get_config("GROUPDB_NAME", "CONNENT_BOT")
-    BOT_NAME = get_config("BOT_NAME", "FILTER2_BOT")
-    SUPPORT_CHAT = get_config('SUPPORT_CHAT', 'mksviplink')
+    SESSION_NAME = get_config("DATABASE_NAME", "MMSUB_BOT")
+    COLLECTION_NAME = get_config("COLLECTION_NAME", "MMSUB1")
+    GROUPDB_NAME = get_config("GROUPDB_NAME", "MMSUBGP_BOT")
+    BOT_NAME = get_config("BOT_NAME", "MMSUB2_BOT")
+    SUPPORT_CHAT = get_config('SUPPORT_CHAT', 'Movie_Group_MMSUB')
     WELCOM_PIC = environ.get("WELCOM_PIC", "")
     WELCOM_TEXT = environ.get("WELCOM_TEXT", "Hay {user}\nwelcome to {chat} GROUP")
     LOG_CHANNEL = int(get_config("LOG_CHANNEL", "-1001254905376"))
    
-    FORCE_SUB_CHANNEL = int(get_config("FORCE_SUB_CHANNEL", "-1001569842499"))
+    FORCE_SUB_CHANNEL = int(get_config("FORCE_SUB_CHANNEL", "-1001832645221"))
 
     TEMPLATE = get_config(
         "IMDB_TEMPLATE",
          """<b>🏷 Title </b>: <a href={url}>{title}</a> -- <a href={url}/releaseinfo>{year}</a>  — <b>{kind}</b> 
  
-📆 Release Info : {release_date}
-🌟 Rating    : <a href={url}/ratings>{rating}</a> / 10 ({votes} user ratings.)
-☀️ Languages : <code>{languages}</code>
-🇲🇲 Subtitles    : Myanmar 🇲🇲 
-📀 RunTime     : {runtime} Minutes
-🎛 Countries    : <code>{countries}</code>
-🎭 Genres        : {genres}
-👥 Cast  : <code>{cast}</code>
-
-ကျွန်တော်တို့ KP & MZ ချန်နယ်လ်မှ 
-ဇာတ်ကားများကြည့်ရှုသည့်အတွက် 
-ကျေးဇူးအများကြီးတင်ပါတယ် ခင်ဗျာ။🙏🙏🙏
-
-</b><a href='https://t.me/Movie_Zone_KP/3'>© MKS & KP Channel</a>""",
+""",
     )
 
     CHANNELS = make_list(get_config("CHANNELS", "-1001832645221"), True)  # type: ignore
@@ -76,15 +63,22 @@ class Config:
 
     CUSTOM_FILE_CAPTION = get_config(
         "CUSTOM_FILE_CAPTION",
-        """{caption}
+        """>📂 Fɪʟᴇ Caption: </b> <code>{caption}</code><b>
+        
+<b>📂 Fɪʟᴇ ɴᴀᴍᴇ : </b> <code>{file_name}</code><b>
 
+<b>📂 Fɪʟᴇ Size :</b> <code>{file_size}</code><b>
 
-ကျွန်တော်တို့ KP & MZ ချန်နယ်လ်မှ 
-ဇာတ်ကားများကြည့်ရှုသည့်အတွက် 
-ကျေးဇူးအများကြီးတင်ပါတယ် ခင်ဗျာ။🙏🙏🙏
+╭─────── • ◆ • ───────╮
+ 🔅 Modified By     :     <a href="https://t.me/kopainglay15">Ko Paing</a>
+ 
+╰─────── • ◆ • ───────╯
+=========== • ✠ • ===========
 
-All Channel Link👉🏻 @Movie_Zone_KP
-</b><a href='https://t.me/Movie_Zone_KP/3'>© MKS & KP Channel</a>""",
+▫️ ᴄʜᴀɴɴᴇʟ : <a href="https://t.me/+4DDoxav12EwyYzA1"> ᴄʟɪᴄᴋ ʜᴇʀᴇ MY_CHANNEL</a>
+▫️ sᴜᴘᴘᴏʀᴛ ɢʀᴏᴜᴘ : <a href="https://t.me/Movie_Group_MMSUB">ᴄʟɪᴄᴋ ʜᴇʀᴇ MY Group</a>
+
+=========== • ✠ • ===========</b>""",
     )
 
     IMDB = True
