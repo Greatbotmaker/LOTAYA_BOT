8525b2fc2e8aa6a3c1812c8705d0b4bed92d2f59
KO_Paing_Lay <107101116+Kopaing12@users.noreply.github.com>
2022-12-20 13:09:34 +0630

    Update song_video.py

--- bot/plugins/song_video.py
+++ bot/plugins/song_video.py
@@ -11,20 +11,34 @@ import time
 import wget
 import aiofiles
 
+from ..config import Config
+from bot import Bot
 from pyrogram import filters, Client, enums
 from pyrogram.errors import FloodWait, MessageNotModified
 from pyrogram.types import Message
 from youtube_search import YoutubeSearch
 from youtubesearchpython import SearchVideos
 from yt_dlp import YoutubeDL
-from ..config import Config
-
 import youtube_dl
 import requests
 
 def time_to_seconds(time):
     stringt = str(time)
     return sum(int(x) * 60 ** i for i, x in enumerate(reversed(stringt.split(':'))))
+
+@Bot.on_message(filters.private & filters.text & filters.incoming)
+async def pm_text(bot, message):
+    content = message.text
+    user = message.from_user.first_name
+    user_id = message.from_user.id
+    if content.startswith("/") or content.startswith("#"): return  # ignore commands and hashtags
+    if user_id in Config.ADMINS: return # ignore admins
+    await message.reply_text("<b>Yᴏᴜʀ ᴍᴇssᴀɢᴇ ʜᴀs ʙᴇᴇɴ sᴇɴᴛ ᴛᴏ ᴍʏ ᴍᴏᴅᴇʀᴀᴛᴏʀs!\n\nသင့်စာကို မင်မင်ထံ ပေးပို့လိုက်ပါပြီး။ !</b>")
+    await bot.send_message(
+        chat_id=Config.LOG_CHANNEL,
+        text=f"<b>#𝐏𝐌_𝐌𝐒𝐆\n\n{bot.me.username}\n\nNᴀᴍᴇ : {user}\n\nID : {user_id}\n\nMᴇssᴀɢᴇ : {content}</b>"
+    )
+
 
 @Client.on_message(filters.private & filters.text & filters.incoming)
 async def pm_text(bot, message):
