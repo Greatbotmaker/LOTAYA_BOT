c37047a81c18c45f65b77d87d227bab279b69074
KO_Paing_Lay <107101116+Kopaing12@users.noreply.github.com>
2022-12-26 21:02:30 +0630

    Update autofilter.py

--- bot/plugins/autofilter.py
+++ bot/plugins/autofilter.py
@@ -70,7 +70,7 @@ G_MODE = {}
 
 @Bot.on_message(filters.group & filters.text & filters.incoming, group=-1)  # type: ignore
 async def give_filter(bot: Bot, message: types.Message):
-    Cache.BUTTONS[key] = search
+    
     settings = await config_db.get_settings(f"SETTINGS_{message.chat.id}")
     if settings["GFILTER"]:
         await global_filters(client, message)
